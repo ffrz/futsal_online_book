86c351b810a091207b488bc95c2f3a058af6d815
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2023-10-31 20:42:53 +0700

    Update sidebar.blade.php

--- resources/views/admin/_components/sidebar.blade.php
+++ resources/views/admin/_components/sidebar.blade.php
@@ -1,25 +1,19 @@
 <aside class="main-sidebar elevation-4 sidebar-dark-lime sidebar-no-expand">
-  <!-- Brand Logo -->
   <a href="#  " class="brand-link">
     <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
       style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
   </a>
-
-  <!-- Sidebar -->
   <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+        <img src="{{ asset('dist/img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
       </div>
     </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
+    {{-- <div class="form-inline">
       <div class="input-group" data-widget="sidebar-search">
         <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
@@ -28,14 +22,10 @@
           </button>
         </div>
       </div>
-    </div>
-
-    <!-- Sidebar Menu -->
+    </div> --}}
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar nav-legacy nav-compact flex-column nav-collapse-hide-child"
         data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
         <li class="nav-item menu-open">
           <a href="#" class="nav-link active">
             <i class="nav-icon fas fa-tachometer-alt"></i>
